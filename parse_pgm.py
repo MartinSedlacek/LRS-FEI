ddd9553852dc3b1585b08141a360cd2f350bdd07
MartinSedlacek <msedlacek.martin@gmail.com>
2023-10-04 09:28:37 +0200

    Add files via upload

---
+++ parse_pgm.py
@@ -0,0 +1,30 @@
+def parse_pgm(data):
+    lines = data.split("\n")
+    metadata = {}
+    pixel_data = []
+
+    # Loop through lines and parse data
+    for line in lines:
+        # Skip comments
+        if line.startswith("#"):
+            continue
+        # Check for magic number P2
+        elif line == "P2":
+            metadata["type"] = "P2"
+        # Check for width and height
+        elif "width" not in metadata:
+            metadata["width"], metadata["height"] = map(int, line.split())
+        # Check for max gray value
+        elif "max_gray" not in metadata:
+            metadata["max_gray"] = int(line)
+        # Parse pixel data
+        else:
+            pixel_data.append(list(map(int, line.split())))
+    return metadata, pixel_data
+
+
+with open("map_080.pgm", "rb") as file:
+    byte_data = file.read()
+    data = byte_data.decode("utf-8")
+metadata, pixel_data = parse_pgm(data)
+print(pixel_data)
